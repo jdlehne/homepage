be31fd83d0a584bf8394401b269c32e21b944b42
Josh Lehne <jdlehne@gmail.com>
2017-11-02 13:14:29 -0500

    working on skills section

--- js/main.js
+++ js/main.js
@@ -26,8 +26,41 @@
     offset: 54
   });
 
+
 })(jQuery); // End of use strict
 
-$("#toggle-btn").click(function(){
-          $("#card-4").flip('toggle');
-        });
+
+// $(function() {
+//    if ($('section #skills:visible')) { // check if there's an element
+//       $('ul#frontList li').addClass('float-up'); // this is how you add class in jquery
+//    }
+// });
+
+//var $animation_elements = $('.float-up');
+var $animation_elements = $('ul#frontList li');
+
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('float-up');
+    } else {
+      $element.removeClass('float-up');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
